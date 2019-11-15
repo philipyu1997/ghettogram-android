842af224d49cee6fbe50ec39d6786c377f4ebac2
Philip Yu <pyu3@mail.sfsu.edu>
2019-11-14 19:56:51 -0800

    Add querying from parse database for user posts

--- app/src/main/java/com/example/ghettogram_android/MainActivity.java
+++ app/src/main/java/com/example/ghettogram_android/MainActivity.java
@@ -3,12 +3,57 @@ package com.example.ghettogram_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.parse.FindCallback;
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String TAG = "MainActivity";
+    private EditText etDescription;
+    private Button btnCaptureImage;
+    private ImageView ivPostImage;
+    private Button btnSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etDescription = findViewById(R.id.etDescription);
+        btnCaptureImage = findViewById(R.id.btnCaptureImage);
+        ivPostImage = findViewById(R.id.ivPostImage);
+        btnSubmit = findViewById(R.id.btnSubmit);
+
+        queryPosts();
     }
+
+    private void queryPosts() {
+        ParseQuery<Post> postQuery = new ParseQuery<Post>(Post.class);
+
+        postQuery.include(Post.KEY_USER);
+        postQuery.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, "Error with query");
+                    e.printStackTrace();
+                    return;
+                }
+
+                for (int i = 0; i < posts.size(); ++i) {
+                    Log.d(TAG, "Post: " + posts.get(i).getDescription() + ", username: " + posts.get(i).getUser().getUsername());
+                }
+            }
+        });
+    }
+
 }
