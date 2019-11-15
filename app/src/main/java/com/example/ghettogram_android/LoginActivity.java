72343fbfa4100b06c085a20234f21c0d58770cc2
Philip Yu <pyu3@mail.sfsu.edu>
2019-11-14 20:51:55 -0800

    User can sign up

--- app/src/main/java/com/example/ghettogram_android/LoginActivity.java
+++ app/src/main/java/com/example/ghettogram_android/LoginActivity.java
@@ -12,6 +12,7 @@ import android.widget.EditText;
 import com.parse.LogInCallback;
 import com.parse.ParseException;
 import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 /**
  * A login screen that offers login via email/password.
@@ -23,6 +24,7 @@ public class LoginActivity extends AppCompatActivity {
     private EditText etUsername;
     private EditText etPassword;
     private Button btnLogin;
+    private Button btnSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +33,7 @@ public class LoginActivity extends AppCompatActivity {
 
         etUsername = findViewById(R.id.etUsername);
         etPassword = findViewById(R.id.etPassword);
+
         btnLogin = findViewById(R.id.btnLogin);
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,6 +43,38 @@ public class LoginActivity extends AppCompatActivity {
                 login(username, password);
             }
         });
+
+        btnSignUp = findViewById(R.id.btnSignUp);
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                signUp(username, password);
+            }
+        });
+    }
+
+    private void signUp(final String username, String password) {
+
+        ParseUser user = new ParseUser();
+
+        user.setUsername(username);
+        user.setPassword(password);
+
+        user.signUpInBackground(new SignUpCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, "Issue with sign up");
+                    e.printStackTrace();
+                    return;
+                } else {
+                    Log.d(TAG, "Successfully registered user with username: " + username);
+                }
+            }
+        });
+
     }
 
     private void login(String username, String password) {
